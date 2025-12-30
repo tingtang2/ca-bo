add simple input transform, add sobol init, fix acq func standardize bug, change gpytorch fast computation settings, begin adding spherical linear kernel

src/trainers/acquisition_fn_trainers.py
@@ -10,6 +10,7 @@ from botorch.acquisition import qExpectedImprovement, qLogExpectedImprovement
 from botorch.acquisition.analytic import ExpectedImprovement
 from botorch.optim import optimize_acqf
 from botorch.optim.initializers import initialize_q_batch_nonneg
+from botorch.utils import standardize
 
 
 class EITrainer(BaseTrainer):
@@ -23,13 +24,19 @@ class EITrainer(BaseTrainer):
         x_center = copy.deepcopy(X[Y.argmax(), :])
         weights = torch.ones_like(x_center)
 
-        lb = self.task.lb * weights
-        ub = self.task.ub * weights
+        if self.turn_on_simple_input_transform:
+            lb = 0 * weights
+            ub = 1 * weights
+        else:
+            lb = self.task.lb * weights
+            ub = self.task.ub * weights
 
         if self.use_analytic_acq_func:
-            ei = ExpectedImprovement(model, Y.max().to(self.device))
+            ei = ExpectedImprovement(model,
+                                     standardize(Y).max().to(self.device))
         else:
-            ei = qExpectedImprovement(model, Y.max().to(self.device))
+            ei = qExpectedImprovement(model,
+                                      standardize(Y).max().to(self.device))
 
         if self.enable_raasp:
             options = {
@@ -61,13 +68,19 @@ class LogEITrainer(BaseTrainer):
         x_center = copy.deepcopy(X[Y.argmax(), :])
         weights = torch.ones_like(x_center)
 
-        lb = self.task.lb * weights
-        ub = self.task.ub * weights
+        if self.turn_on_simple_input_transform:
+            lb = 0 * weights
+            ub = 1 * weights
+        else:
+            lb = self.task.lb * weights
+            ub = self.task.ub * weights
 
         if self.use_analytic_acq_func:
-            ei = LogExpectedImprovement(model, Y.max().to(self.device))
+            ei = LogExpectedImprovement(model,
+                                        standardize(Y).max().to(self.device))
         else:
-            ei = qLogExpectedImprovement(model, Y.max().to(self.device))
+            ei = qLogExpectedImprovement(model,
+                                         standardize(Y).max().to(self.device))
 
         if self.enable_raasp:
             options = {

src/trainers/base_trainer.py
@@ -4,7 +4,7 @@ import logging
 from abc import ABC, abstractmethod
 from datetime import datetime
 from pathlib import Path
-from typing import List, Union
+from typing import List, Union, Tuple
 
 import matplotlib.pyplot as plt
 import torch
@@ -233,8 +233,10 @@ class BaseTrainer(ABC):
                 cos_sim_incum,
                 'action_norm':
                 action_norm,
+                # 'log det K(z, z)':
+                # self.calc_log_det_kernel_ips(),
                 'log det K(z, z)':
-                self.calc_log_det_kernel_ips(),
+                -1,
                 'x_af_val':
                 x_af_val,
                 'x_next_sigma':
@@ -271,8 +273,10 @@ class BaseTrainer(ABC):
                 cos_sim_incum,
                 'action_norm':
                 action_norm,
+                # 'S^TK^hatS condition number':
+                # self.calc_cond_num_SKS(),
                 'S^TK^hatS condition number':
-                self.calc_cond_num_SKS(),
+                -1,
                 'x_af_val':
                 x_af_val,
                 'x_next_sigma':
@@ -493,3 +497,18 @@ class BaseTrainer(ABC):
         plt.legend()
         plt.tight_layout()
         plt.savefig(f'{self.name}_.png')
+
+    def sobol_initialize_data(self) -> Tuple[torch.tensor, torch.tensor]:
+        sobol = torch.quasirandom.SobolEngine(dimension=self.task.dim,
+                                              scramble=True,
+                                              seed=self.seed)
+        sobol_draws = sobol.draw(n=self.num_initial_points).to(self.device)
+        if self.turn_on_simple_input_transform:
+            init_train_x = sobol_draws
+        else:
+            init_train_x = sobol_draws * (self.task.ub -
+                                          self.task.lb) + self.task.lb
+        init_train_y = self.task(sobol_draws * (self.task.ub - self.task.lb) +
+                                 self.task.lb)
+
+        return init_train_x, init_train_y

src/trainers/svgp_trainer.py
@@ -159,7 +159,10 @@ class SVGPTrainer(BaseTrainer):
     def run_experiment(self, iteration: int):
         # get all attribute information
         logging.info(self.__dict__)
-        train_x, train_y = self.initialize_data()
+        if self.turn_on_sobol_init:
+            train_x, train_y = self.sobol_initialize_data()
+        else:
+            train_x, train_y = self.initialize_data()
 
         self.train_y_mean = train_y.mean()
         self.train_y_std = train_y.std()
@@ -273,7 +276,11 @@ class SVGPTrainer(BaseTrainer):
             standardized_gain = (x_next_mu - torch.max(train_y)) / x_next_sigma
 
             # Evaluate candidates
-            y_next = self.task(x_next)
+            if self.turn_on_simple_input_transform:
+                y_next = self.task(x_next * (self.task.ub - self.task.lb) +
+                                   self.task.lb)
+            else:
+                y_next = self.task(x_next)
 
             # Update data
             train_x = torch.cat((train_x, x_next), dim=-2)